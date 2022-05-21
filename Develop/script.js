df2fa99733f17703ceac2de0042030cb35531f8f
Montez Burt <Griffenc4@gmail.com>
2022-05-21 02:58:43 -0500

    added random functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,29 @@
 // Assignment code here
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+ 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#^&*(){}[]=<>,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  
+
+  return "Generated password will go here"
+}
 
 // Write password to the #password input
 function writePassword() {
