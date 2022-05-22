3a70a12512d0ae50cdc3bad7c213837b13ead979
Montez Burt <Griffenc4@gmail.com>
2022-05-22 18:02:13 -0500

    added return after generated password to return the password to what initially called it

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,7 @@
 // Assignment code here
-//const randomFunc = {
- // lower: getRandomLower
- // upper: getRandomUpper
- // number: getRandomNumber
- // symbol: getRandomSymbol
-//}
-//function getRandomLower() {
-  //return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-//}
-
-//function getRandomUpper() {
- // return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-//}
- 
-//function getRandomNumber() {
- // return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-//}
-
-//function getRandomSymbol() {
-//  const symbols = '!@#^&*(){}[]=<>,.';
- // return symbols[Math.floor(Math.random() * symbols.length)];
-//}
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+// functions used to generate random list
 function randomInt(min, max) {
   if (!max) {
     max= min
@@ -54,22 +31,20 @@ function generatePassword() {
     window.alert("Password length must be between 8 to 128 characters")
     return
   }
-
+//window prompts for user to pick password criteria
   var userIncludesNumbers = window.confirm("Would you like your password to include numbers?")
   var userIncludesLowercase = window.confirm("Would you like your password to include lowercase letters?")
   var userIncludesUppercase = window.confirm("Would you like your password to include uppercase letters?")
   var userIncludesSpecial = window.confirm("Would you like your password to include special characters?")
-
+// password lists
   var numberList = ["0","1","2","3","4","5","6","7","8","9","10"]
-  var lowercaseList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","x","z",]
-  var uppercaseList = []
+  var lowercaseList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","x","z"]
+  var uppercaseList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","X","Z"]
   var specialList = ["!","@","#","^","&","*","(",")","{","}","[", "]","=","<",">",",","."]
 
   var optionsSelect = []
 
-  for (var i = 0; i < lowercaseList.length; i++) {
-    uppercaseList[i] = lowercaseList[i].toUpperCase
-  }
+  
 
   if (userIncludesNumbers) {
     optionsSelect.push(numberList)
@@ -97,6 +72,7 @@ function generatePassword() {
     generatedPassword += randomChar
   }
  
+  return generatedPassword
  
 
  
